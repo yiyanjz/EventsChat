94a2dec77f5c3677b273a448edf8da6888bc38db
Justin Zhang <justinzhang321@gmail.com>
2023-12-17 13:28:55 -0800

    no location details view

---
+++ EC/Core/UploadActions/View/NoLocationDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoLocationDetailsView.swift
+//  EC
+//
+//  Created by Justin Zhang on 12/17/23.
+//
+
+import SwiftUI
+
+struct NoLocationDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoLocationDetailsView()
+}
