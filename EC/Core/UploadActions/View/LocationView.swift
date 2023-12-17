d601479452cf1bc4d54b26ff4005c80e7548c54d
Justin Zhang <justinzhang321@gmail.com>
2023-12-17 13:25:50 -0800

    locationview

---
+++ EC/Core/UploadActions/View/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  EC
+//
+//  Created by Justin Zhang on 12/16/23.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
